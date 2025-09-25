d2266e1535797a946a9e8c95872daf0b04972290
prateekarya100 <prateekarya100@gmail.com>
2025-09-25 20:31:09 +0530

    commit changes into updated project

--- ecommerce/src/main/java/com/tomcat/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/tomcat/ecommerce/EcommerceApplication.java
@@ -1,11 +1,7 @@
 package com.tomcat.ecommerce;
 
-import com.tomcat.ecommerce.controller.CategoryController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class EcommerceApplication {
--- ecommerce/src/main/java/com/tomcat/ecommerce/service/category/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.tomcat.ecommerce.service.category;
-
-import com.tomcat.ecommerce.models.category.Category;
-
-import java.util.List;
-
-public interface CategoryService {
-    List<Category> getAllCategories();
-}
