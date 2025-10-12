0791e0785c008a26edc2577da2b0f62f137e4455
prateekarya100 <prateekarya100@gmail.com>
2025-10-12 20:36:26 +0530

    commit changes into ecommerce

--- media/src/main/java/com/social/media/models/Posts.java
+++ media/src/main/java/com/social/media/models/Posts.java
@@ -2,12 +2,18 @@ package com.social.media.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
 @Entity
-public class SocialProfile {
+public class Posts {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(mappedBy = "socialProfile")
+    @ManyToOne
+    @JoinColumn(name = "social_user_id", referencedColumnName = "id")
     private SocialUser socialUser;
 }
--- media/src/main/java/com/social/media/models/SocialUser.java
+++ media/src/main/java/com/social/media/models/SocialUser.java
@@ -1,14 +1,29 @@
 package com.social.media.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class SocialUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "social_profile_id", referencedColumnName = "id")
+    private SocialProfile socialProfile;
+
+    @OneToMany(mappedBy = "socialUser", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Posts> posts = new ArrayList<>();
+
+    //many to many with social groups
+    @ManyToMany
+    @JoinTable(
+            name = "user_groups",
+            joinColumns = @JoinColumn(name = "social_user_id"),
+            inverseJoinColumns = @JoinColumn(name = "social_group_id")
+    )
+    private List<SocialGroup> socialGroups = new ArrayList<>();
 }
