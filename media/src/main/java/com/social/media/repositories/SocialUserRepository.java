df98e1fa58aad2c1823dd5fd896eecb58c716503
prateekarya100 <prateekarya100@gmail.com>
2025-10-16 15:58:49 +0530

    commit changes into social media

--- media/src/main/java/com/social/media/repositories/SocialUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.social.media.repositories;
-
-import com.social.media.models.SocialUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SocialUserRepository extends JpaRepository<SocialUser,Long> {
-}
--- media/src/main/java/com/social/media/service/SocialGroupService.java
+++
@@ -1,7 +0,0 @@
-package com.social.media.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class SocialGroupService {
-}
