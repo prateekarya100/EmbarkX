e8f4e2cd3abf9896154a4e9ad4311f1ce7ef4745
prateekarya100 <prateekarya100@gmail.com>
2025-10-30 06:00:17 +0530

    commit into ecommerce

---
+++ security/src/main/java/security/jwt/JwtUtils.java
@@ -0,0 +1,95 @@
+package security.jwt;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(JwtUtils.class);
+
+    @Value("${myapp.jwtSecretKey}")
+    private String jwtSecretKey;
+
+    @Value("${myapp.jwtExpirationMs}")
+    private int jwtExpirationMs = 3600000; // 1 hour
+
+    // Method to extract JWT token from Authorization header
+    public String getJwtFromHeader(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    // Method to generate JWT token from username
+    public String generateFromUsername(String username) {
+        long now = System.currentTimeMillis();
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date(now))
+                .expiration(new Date(now + jwtExpirationMs))
+                .signWith(key())
+                .compact();
+    }
+
+    // Method to extract username from JWT token
+    public String getUsernameFromJwt(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+
+    // Method to sign JWT token with secret key
+    public Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecretKey));
+    }
+
+    // Method to validate JWT token
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser()
+                    .verifyWith((SecretKey) key())
+                    .build()
+                    .parseSignedClaims(authToken);
+            return true;
+        }catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+
+
+
+    // Method to check if JWT token is expired
+    public boolean isTokenExpired(String token){
+        Date expiration = Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().getExpiration();
+        return expiration.before(new Date());
+    }
+
+    // Method to check if JWT token is valid
+}
